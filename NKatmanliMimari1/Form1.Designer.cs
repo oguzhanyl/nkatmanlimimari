a5785301ef85c7626c2d2534ac48332bd5fc18ce
Oguzhan Yilmaz <oguzhan06_61@hotmail.com>
2023-07-15 14:50:44 +0300

    N katmanli mimari kullanilarak personel kayit programi olusturuldu.

---
+++ NKatmanliMimari1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace NKatmanliMimari1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Txtad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Txtsoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txtsehir = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txtmaas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Txtgorev = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BTNEKLE = new System.Windows.Forms.Button();
+            this.Btnsil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(663, 201);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(437, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LİSTELE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(43, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(73, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Txtad
+            // 
+            this.Txtad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txtad.Location = new System.Drawing.Point(73, 44);
+            this.Txtad.Name = "Txtad";
+            this.Txtad.Size = new System.Drawing.Size(132, 26);
+            this.Txtad.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(37, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "AD:";
+            // 
+            // Txtsoyad
+            // 
+            this.Txtsoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txtsoyad.Location = new System.Drawing.Point(73, 76);
+            this.Txtsoyad.Name = "Txtsoyad";
+            this.Txtsoyad.Size = new System.Drawing.Size(132, 26);
+            this.Txtsoyad.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(3, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "SOYAD:";
+            // 
+            // Txtsehir
+            // 
+            this.Txtsehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txtsehir.Location = new System.Drawing.Point(289, 44);
+            this.Txtsehir.Name = "Txtsehir";
+            this.Txtsehir.Size = new System.Drawing.Size(132, 26);
+            this.Txtsehir.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(219, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ŞEHİR:";
+            // 
+            // Txtmaas
+            // 
+            this.Txtmaas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txtmaas.Location = new System.Drawing.Point(289, 76);
+            this.Txtmaas.Name = "Txtmaas";
+            this.Txtmaas.Size = new System.Drawing.Size(132, 26);
+            this.Txtmaas.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(224, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "MAAŞ:";
+            // 
+            // Txtgorev
+            // 
+            this.Txtgorev.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txtgorev.Location = new System.Drawing.Point(289, 12);
+            this.Txtgorev.Name = "Txtgorev";
+            this.Txtgorev.Size = new System.Drawing.Size(132, 26);
+            this.Txtgorev.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(211, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "GÖREV:";
+            // 
+            // BTNEKLE
+            // 
+            this.BTNEKLE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BTNEKLE.Location = new System.Drawing.Point(437, 47);
+            this.BTNEKLE.Name = "BTNEKLE";
+            this.BTNEKLE.Size = new System.Drawing.Size(118, 33);
+            this.BTNEKLE.TabIndex = 14;
+            this.BTNEKLE.Text = "EKLE";
+            this.BTNEKLE.UseVisualStyleBackColor = true;
+            this.BTNEKLE.Click += new System.EventHandler(this.BTNEKLE_Click);
+            // 
+            // Btnsil
+            // 
+            this.Btnsil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btnsil.Location = new System.Drawing.Point(561, 9);
+            this.Btnsil.Name = "Btnsil";
+            this.Btnsil.Size = new System.Drawing.Size(118, 33);
+            this.Btnsil.TabIndex = 15;
+            this.Btnsil.Text = "SİL";
+            this.Btnsil.UseVisualStyleBackColor = true;
+            this.Btnsil.Click += new System.EventHandler(this.Btnsil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGuncelle.Location = new System.Drawing.Point(561, 47);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(118, 33);
+            this.BtnGuncelle.TabIndex = 16;
+            this.BtnGuncelle.Text = "GÜNCELLE";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 354);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.Btnsil);
+            this.Controls.Add(this.BTNEKLE);
+            this.Controls.Add(this.Txtgorev);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Txtmaas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Txtsehir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Txtsoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Txtad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Txtad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Txtsoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txtsehir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txtmaas;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Txtgorev;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BTNEKLE;
+        private System.Windows.Forms.Button Btnsil;
+        private System.Windows.Forms.Button BtnGuncelle;
+    }
+}
+
---
+++ NKatmanliMimari1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAccessLayer;
+using EntityLayer;
+using LogicLayer;
+
+namespace NKatmanliMimari1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<EntityPersonel> PerList = LogicPersonel.LLPersonelListesi();
+            dataGridView1.DataSource = PerList;
+        }
+
+        private void BTNEKLE_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Ad = Txtad.Text;
+            ent.Soyad = Txtsoyad.Text;
+            ent.Maas = short.Parse(Txtmaas.Text);
+            ent.Gorev = Txtgorev.Text;
+            ent.Sehir = Txtsehir.Text;
+            LogicPersonel.LLPersonelEkle(ent);
+        }
+
+        private void Btnsil_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(textBox1.Text);
+            LogicPersonel.LLPersonelSil(ent.Id);
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            EntityPersonel ent = new EntityPersonel();
+            ent.Id = Convert.ToInt32(textBox1.Text);
+            ent.Ad = Txtad.Text;
+            ent.Soyad = Txtsoyad.Text;
+            ent.Maas = short.Parse(Txtmaas.Text);
+            ent.Gorev = Txtgorev.Text;
+            ent.Sehir = Txtsehir.Text;
+            LogicPersonel.LLPersonelGuncelle(ent);
+        }
+    }
+}
